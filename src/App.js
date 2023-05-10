5c1f7f5cc25dcbcb7f9daa8cfe564c4a62379e83
DanBaDo <bytes4life@proton.me>
2023-05-10 18:12:47 +0200

    Provides views and SPA routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; 
 import './App.css';
 import Portada from './vistas/Portada';
 import VistaDatos from './vistas/VistaDatos';
@@ -7,12 +8,23 @@ import { ServicioOfertas } from './servicios/ServicioOfertas';
 function App() {
   return (
   <ServicioOfertas>
-  <h1>React Flux example</h1>
-  <main>
-    <Portada/>
-    <Backoffice/>
-    <VistaDatos/>
-  </main>
+  <BrowserRouter>
+    <h1>React Flux example</h1>
+    <nav>
+      <ul>
+        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/ofertas/">Ofertas</Link></li>
+        <li><Link to="/backoffice/">Backoffice</Link></li>
+      </ul>
+    </nav>
+    <main>
+      <Routes>
+        <Route path='/' element={<Portada/>}/>
+        <Route path='/backoffice/' element={<Backoffice/>}/>
+        <Route path='/ofertas/' element={<VistaDatos/>}/>
+      </Routes>
+    </main>
+  </BrowserRouter>
   </ServicioOfertas>
   );
 }
